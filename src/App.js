91a5e54391c12d505da044bacfa0b58cff15a424
Mario Salles <mariofsalles@gmail.com>
2019-03-02 14:07:34 +0000

    Detach components: Header, Home, Services and Footer (functional staless components)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 import Site from './Site'
+
+import Header from './components/Header';
+import Home from './components/Home';
 import Footer from './components/Footer';
+import Services from './components/Services';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
+        <Home/>
+        <Services/>
         <Site/>
         <Footer/>
       </div>
